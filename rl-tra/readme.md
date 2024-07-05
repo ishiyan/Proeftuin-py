d07fc6979bc646866048c5e3445c188848c83287
ivas <ivas@saxobank.com>
2024-07-05 16:45:18 +0200

    update

--- rl-tra/readme.md
+++ rl-tra/readme.md
@@ -1,1 +1,9 @@
-ECHO is on.
+# RL experiments
+
+## PPO
+
+[Stable Baseline3](https://stable-baselines3.readthedocs.io/en/master/modules/ppo.html) implementation
+of the [Proximal Policy Optimization, 2017](https://arxiv.org/abs/1707.06347) algorithm:
+`sb3_ppo_01_train.py`.
+
+
--- rl-tra/sb3/sb3_callbacks.py
+++ rl-tra/sb3/sb3_callbacks.py
@@ -30,8 +30,8 @@ class SaveOnBestTrainingRewardCallback(BaseCallback):
 
     def _init_callback(self) -> None:
         # Create folder if needed.
-        if self.save_path is not None:
-            os.makedirs(self.save_path, exist_ok=True)
+        if self.log_dir is not None:
+            os.makedirs(self.log_dir, exist_ok=True)
 
     def _on_step(self) -> bool:
         if self.n_calls % self.check_freq == 0:
