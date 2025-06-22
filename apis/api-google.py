ad2e9f13c78dd350a361ef8a447c5283329d7010
ishiyan <ivan.shiyan@gmail.com>
2025-06-22 12:32:58 +0200

    google api

--- apis/api-google.py
+++ apis/api-google.py
@@ -8,6 +8,6 @@ import os
 #genai.configure(api_key="", transport="rest")
 genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash")
 response = model.generate_content("Write a story about a magic backpack.")
 print(response.text)
